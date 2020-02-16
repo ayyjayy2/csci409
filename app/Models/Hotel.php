b8db31e745b882cbd062c97c36e11cb351467682
ayyjayy2 <55775759+ayyjayy2@users.noreply.github.com>
2020-02-16 11:20:41 -0500

    Create Hotel.php

---
+++ app/Models/Hotel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Hotel extends Model
+{
+    //code to allow mass assignment
+    protected $fillable =[
+      'name',
+      'location',
+      'description',
+      'image'
+    ];
+    //set up hasMany side of our hotel -> rooms relation
+    public function rooms() {
+      return $this->hasMany('App\Models\Room');
+    }
+}
