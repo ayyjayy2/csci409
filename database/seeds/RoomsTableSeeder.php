693caf0e2db6da6676c4afa41adf214fbbcdd472
ayyjayy2 <55775759+ayyjayy2@users.noreply.github.com>
2020-02-22 14:05:06 -0500

    Created Rooms Seeder

--- database/seeds/RoomsTableSeeder.php
+++ database/seeds/RoomsTableSeeder.php
@@ -60,47 +60,5 @@ class RoomsTableSeeder extends Seeder
             'image' => $room['image']
           ));
         }
-
-        /*//array of room bookings to be created
-        = [
-            [
-                'user_id' => '1', //check how to do foreign key
-                'room_id' => '1', //check how to do foreign key
-                'num_of_guests' => '4' //check how to do integer
-                'arrival' => '5/18/2020', //check how to do date
-                'departure' => '5/28/2020'
-            ],
-            [
-                'user_id' => '1',
-                'room_id' => '2',
-                'num_of_guests' => '1'
-                'arrival' => '5/10/2020',
-                'departure' => '5/12/2020'
-            ],
-            [
-                'user_id' => '1',
-                'room_id' => '3',
-                'num_of_guests' => '3'
-                'arrival' => '5/6/2020',
-                'departure' => '5/7/2020'
-            ],
-            [
-                'user_id' => '1',
-                'room_id' => '4',
-                'num_of_guests' => '2'
-                'arrival' => '5/12/2020',
-                'departure' => '5/15/2020'
-            ],
-            [
-                'user_id' => '1',
-                'room_id' => '2',
-                'num_of_guests' => '2'
-                'arrival' => '5/20/2020',
-                'departure' => '5/24/2020'
-            ],
-          ]; //end array
-
-          //loop through bookings array
-          */
     }
 }
