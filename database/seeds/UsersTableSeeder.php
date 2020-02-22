9705adcde2bb2a68361ce07529c4e3bd25b4f613
ayyjayy2 <55775759+ayyjayy2@users.noreply.github.com>
2020-02-22 12:43:23 -0500

    Create Users seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+/*
+this file is simply to get a user in for the foreign key constraint on the reservations table
+*/
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
