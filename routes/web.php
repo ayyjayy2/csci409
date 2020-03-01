39b5ab1362f125aed486b348b45634dbc6a2418b
ayyjayy2 <55775759+ayyjayy2@users.noreply.github.com>
2020-02-29 19:30:03 -0500

    Added routes to web.php

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,21 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return '/route';
 });
+
+Route::get('hotels', function(){
+    return '/hotels route';
+});
+
+Route::group(['prefix' => 'reservations'], function(){
+    Route::get('/', function(){return 'Showing users homepage';});
+    Route::get('reservations', function(){return 'Showing users reservations';});
+    Route::get('reservations/new', function(){return 'Showing form to create reservations';});
+    Route::get('reservations/{id}', function($id){return 'Showing reservations id';});
+    Route::get('reservations/{id}/edit', function($id){return 'Showing edit form for reservation id';});
+
+    Route::post('reservations', function(){return 'Creating reservation';});
+    Route::put('reservations/{id}', function($id){return 'Updating reservations id';});
+    Route::delete('reservations/{id}', function($id){return 'Deleting reservation id';});
+})
